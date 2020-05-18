e498e94636bee3b759b43d1b969affcc7a3aa1b2
Majon1 <mathilda.nynas@gmail.com>
2020-05-18 15:34:13 +0300

    part 2 done

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -8,7 +8,7 @@ namespace Part2
         public static void Main(string[] args)
         {
 
-            /* EX 1
+            //EX 1
             Counter counter = new Counter();
             string input10 = counter.CreateInput(10);
             string input100 = counter.CreateInput(100);
@@ -30,33 +30,33 @@ namespace Part2
 
 
             //EX 2
-             LongestRepetition p = new LongestRepetition();
-             Console.WriteLine(p.Calculate(new int[] { 1, 2, 1, 1, 2 })); // 2
-             Console.WriteLine(p.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
-             Console.WriteLine(p.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 5
-             Console.WriteLine(p.Calculate(new int[] { 5, 5, 1, 1, 1 })); // 3
-             
+            LongestRepetition p = new LongestRepetition();
+            Console.WriteLine(p.Calculate(new int[] { 1, 2, 1, 1, 2 })); // 2
+            Console.WriteLine(p.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
+            Console.WriteLine(p.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 5
+            Console.WriteLine(p.Calculate(new int[] { 5, 5, 1, 1, 1 })); // 3
 
-             //EX3
-             Changes m = new Changes();
-             Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
-             Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
-             Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2
-             */
+
+            //EX3
+            Changes m = new Changes();
+            Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
+            Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
+            Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2
+
             //EX4
-            
-                Revolutions r = new Revolutions();
-                Console.WriteLine(r.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
-                Console.WriteLine(r.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
-                Console.WriteLine(r.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
-                /*
-                //EX5
-                Split s = new Split();
-                Console.WriteLine(s.Calculate(new int[] { 1, 2, -1, 4, 0 })); // 1
-                Console.WriteLine(s.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
-                Console.WriteLine(s.Calculate(new int[] { 0, 0, 0, 0, 0 })); // s
-    */
-            }
-        }
 
+            Revolutions r = new Revolutions();
+            Console.WriteLine(r.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
+            Console.WriteLine(r.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
+            Console.WriteLine(r.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
+
+            //EX5
+            Split s = new Split();
+            Console.WriteLine(s.Calculate(new int[] { 1, 2, -1, 4, 0 })); // 1
+            Console.WriteLine(s.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
+            Console.WriteLine(s.Calculate(new int[] { 0, 0, 0, 0, 0 })); // 4
+
+        }
     }
+
+}
