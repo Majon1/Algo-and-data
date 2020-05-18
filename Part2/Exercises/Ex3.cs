65738104d33300f2a9628720481196bb329aa4fa
Majon1 <mathilda.nynas@gmail.com>
2020-05-18 12:04:41 +0300

    ex 3 done

--- Part2/Exercises/Ex3.cs
+++ Part2/Exercises/Ex3.cs
@@ -3,17 +3,25 @@ using System;
 namespace Part2
 {
     public class Changes
-    { 
+    {
         public int Calculate(int[] t)
-        { int sum = 0;
+        {
+            int sum = 0;
             for (int i = 1; i <= t.Length - 1; i++)
             {
                 if (t[i] == t[i - 1]) //&& (t[i] == t[i + 1]))
                 {
-                   t[i] = t[i - 1] + 3;
                     sum++;
+                    
+                    if (i < t.Length - 1)
+                    {
+                        t[i] = t[i - 1] + t[i + 1] + 1;
+                    }
+                    else
+                    {
+                        t[i] = t [i - 1] - t[i];
+                    }
                 }
-                
             }
             return sum;
         }
--- Part2/Program.cs
+++ Part2/Program.cs
@@ -40,7 +40,7 @@ namespace Part2
             Changes m = new Changes();
             Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
             Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
-            Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1, 1})); // 2
+            Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2
 
         }
     }
