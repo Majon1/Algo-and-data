54483e27cd2987ac65ab0d0e6b1af6a6b4c065d1
Majon1 <mathilda.nynas@gmail.com>
2020-05-29 13:04:24 +0300

    ex 2 done

--- Part4/Program.cs
+++ Part4/Program.cs
@@ -6,16 +6,24 @@ namespace Part4
     {
         public static void Main(string[] args)
         {
-            //EX1
-            LinkedList myLinks = new LinkedList();
-            myLinks.AddLast(1);
-            myLinks.AddFirst(2);
-            myLinks.AddLast(3);
-            Console.WriteLine(myLinks); // for example: 2, 1, 3
-            myLinks.RemoveFirst();
-            Console.WriteLine(myLinks); // for example: 1, 3
-            Console.WriteLine(myLinks.GetNode(0)); // 1
-            Console.WriteLine(myLinks.GetNode(1)); // 3
+            /* //EX1
+             LinkedList myLinks = new LinkedList();
+             myLinks.AddLast(1);
+             myLinks.AddFirst(2);
+             myLinks.AddLast(3);
+             Console.WriteLine(myLinks); // for example: 2, 1, 3
+             myLinks.RemoveFirst();
+             Console.WriteLine(myLinks); // for example: 1, 3
+             Console.WriteLine(myLinks.GetNode(0)); // 1
+             Console.WriteLine(myLinks.GetNode(1)); // 3 */
+
+            Console.WriteLine();
+
+            //EX2
+            CircleGame g = new CircleGame();
+            Console.WriteLine(g.Last(7)); // 7
+            Console.WriteLine(g.Last(4)); // 1
+            Console.WriteLine(g.Last(123)); // 119
         }
     }
 
