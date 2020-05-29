81fc87d3ff4a6cebc04403142217bc21043b18de
Majon1 <mathilda.nynas@gmail.com>
2020-05-29 13:07:13 +0300

    ex 1 done, ex 2 done

---
+++ Part4/Exercises/Ex2.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Part4
+{
+    public class CircleGame
+    {
+        public int Last(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            if (n % 2 == 0)
+            {
+                return(2 * Last(n / 2) - 1);
+            }
+            else
+            {
+                return(2 * Last((n - 1) / 2) + 1);
+            }
+            
+        }
+    }
+}
