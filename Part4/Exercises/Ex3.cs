cbe97d318178faa6a05d70ac023e1203d0b7991c
Majon1 <mathilda.nynas@gmail.com>
2020-05-29 13:41:51 +0300

    ex 3 done

---
+++ Part4/Exercises/Ex3.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Part4
+{
+    public class SmallestDistance
+    { List<int> numbers = new List<int>();
+        public void Add(int x)
+        {
+            numbers.Add(x);
+        } 
+        public int Calculate()
+        {
+            numbers.Sort();
+            int minDiff = numbers[1] - numbers[0];
+            for (int i = 2; i != numbers.Count; i++)
+            {
+                minDiff = Math.Min(minDiff, numbers[i] - numbers[i - 1]);
+            }
+            return minDiff;
+        }   
+    }
+}
