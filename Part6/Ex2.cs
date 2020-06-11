640e65b670e66c462e1e068ea632aa643f26f798
Majon1 <mathilda.nynas@gmail.com>
2020-06-11 13:32:49 +0300

    ex 2 structure

--- Part6/Ex2.cs
+++ Part6/Ex2.cs
@@ -1,12 +1,13 @@
-/*using System;
+using System;
 
 namespace Part6
 {
     public class Dijkstra
     {
+        public int n;
         public Dijkstra(int n)
         {
-
+        this.n = n;
         }
         public void AddRoad(int a, int b, int d)
         {
