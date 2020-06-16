ff2e9889cd6e8e4a94ab319e99fca4e5b7da38d2
Majon1 <mathilda.nynas@gmail.com>
2020-06-16 22:18:41 +0300

    ex 1 & 3 done, not ex 2 & 4

--- Part6/Ex2.cs
+++ Part6/Ex2.cs
@@ -1,21 +1,80 @@
 /*using System;
+using System.Collections.Generic;
 
 namespace Part6
 {
     public class Dijkstra
     {
         public int n;
+        public int[,] adjMatrix;
+        public int[,] parent;
+
         public Dijkstra(int n)
         {
-        this.n = n;
+            this.n = n;
+            this.adjMatrix = new int[n + 1, n + 1];
+            this.parent = new int[n + 1, n + 1];
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    if (i == j)
+                    {
+                        adjMatrix[i, j] = 0;
+                    }
+                    else
+                    {
+                        adjMatrix[i, j] = int.MaxValue;
+                    }
+                }
+            }
         }
         public void AddRoad(int a, int b, int d)
         {
-
+            adjMatrix[a, b] = d;
         }
         public int Calculate(int x, int y)
         {
-            
+            int infinity = 9999;
+            int[,] distance = new int[n + 1, n + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    distance[i, j] = adjMatrix[i, j];
+                    if (adjMatrix[i, j] != int.MaxValue && i != j)
+                    {
+                        parent[i, j] = i;
+                    }
+                    else
+                    {
+                        parent[i, j] = -1;
+                    }
+                }
+            }
+            foreach (int node in adjMatrix)
+            {
+                distance[node, y] = infinity;
+                distance[node - 1, y] = 0;
+            }
+            distance[x, y] = 0;
+            while (adjMatrix.Length > 0)
+            {
+                foreach (node + 1 && LinkedListNode - 1)
+                {
+                    int alt = distance[n] + distance[n, n + 1];
+                }
+            }
+
+            if (distance[x, y] == infinity)
+            {
+                return -1;
+            }
+            else
+            {
+                return distance[x, y];
+            }
         }
     }
 }*/
--- Part6/Program.cs
+++ Part6/Program.cs
@@ -12,18 +12,18 @@ namespace Part6
             s.AddRoad(1, 3, 6);
             s.AddRoad(3, 4, 5);
             s.AddRoad(4, 5, 3);
-          //s.AddRoad(6, 7, 3);
+            //s.AddRoad(6, 7, 3);
             Console.WriteLine(s.Calculate(1, 5)); // 12
 
-            /*Console.WriteLine();
+            Console.WriteLine();
             //EX2
-            Dijkstra s = new Dijkstra(5);
-            s.AddRoad(1, 2, 7);
-            s.AddRoad(2, 4, 2);
-            s.AddRoad(1, 3, 6);
-            s.AddRoad(3, 4, 5);
-            s.AddRoad(4, 5, 3);
-            Console.WriteLine(s.Calculate(1, 5)); // 12 */
+          /*  Dijkstra d = new Dijkstra(5);
+            d.AddRoad(1, 2, 7);
+            d.AddRoad(2, 4, 2);
+            d.AddRoad(1, 3, 6);
+            d.AddRoad(3, 4, 5);
+            d.AddRoad(4, 5, 3);
+            Console.WriteLine(d.Calculate(1, 5)); // 12 */
 
             Console.WriteLine();
             //EX3
@@ -37,6 +37,14 @@ namespace Part6
 
             Console.WriteLine();
             //EX4
+           /* ShortestPath k = new ShortestPath(5);
+            k.AddRoad(1, 2, 7);
+            k.AddRoad(2, 4, 2);
+            k.AddRoad(1, 3, 6);
+            k.AddRoad(3, 4, 5);
+            k.AddRoad(4, 5, 3);
+            k.Create(1, 5).ForEach(Console.Write); // 1245*/
+
         }
     }
 }
