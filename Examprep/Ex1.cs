7309b42f5f76633f306a915f5acdd5df846b039a
Majon1 <mathilda.nynas@gmail.com>
2020-10-03 10:51:05 +0300

    examprep ex 1 done

--- Examprep/Ex1.cs
+++ Examprep/Ex1.cs
@@ -1,9 +1,22 @@
 using System;
-
 namespace Examprep
 {
-    public class ex1
-    {
+    public class Numbers
+    {    
+       public int Steps(int x)
+       {
+        int i = 1;
+        int sum = 0;
+        int count = 0;
+        
+        while( sum < x)
+        {
+            sum = sum + i;
+            i++;
+            count++;
+        }
+        return count;
+       }
 
     }
 }
--- Examprep/Program.cs
+++ Examprep/Program.cs
@@ -1,12 +1,15 @@
 using System;
 
-namespace Part1
+namespace Examprep
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            Numbers num = new Numbers();
+            Console.WriteLine(num.Steps(11)); // 5
+            Console.WriteLine(num.Steps(15)); // 5
+            Console.WriteLine(num.Steps(2)); // 2
         }
     }
 }
