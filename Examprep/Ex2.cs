fcab2839e8b99b122fb83bd7b8767190f76766e7
Majon1 <mathilda.nynas@gmail.com>
2020-10-04 21:22:45 +0300

    up

--- Examprep/Ex2.cs
+++ Examprep/Ex2.cs
@@ -2,8 +2,29 @@ using System;
 
 namespace Examprep
 {
-    public class ex2
+    public class ShortenArray
     {
+        public int Calculate(int[] input)
+        {
+           int n = input.Length;
+           int i=0;
+           int k=0;
 
+           for (i = 0; i<(n-1);)
+           {
+               if((input[i] == input[i+1]) && (input[i] == input[i + 2]))
+               {
+                   for(k=i+1;k<(n-1);k++)
+                   {
+                       input[k]=input[k+1];
+                   }
+                   --n;
+               }
+               else
+               i++;
+           }
+           return n;
+        }
     }
 }
+////SORTED
